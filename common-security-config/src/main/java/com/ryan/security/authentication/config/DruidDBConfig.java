9cbf28732e36b5caa8a23e007d8485b9aa7beb41
shryan-bin <chuan.cheng@51bestfuneral.com>
2018-09-26 23:58:52 +0800

    initial project

---
+++ common-security-config/src/main/java/com/ryan/security/authentication/config/DruidDBConfig.java
@@ -0,0 +1,171 @@
+package com.ryan.security.authentication.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "common.datasource")
+public class DruidDBConfig {
+
+    private String url;
+
+    private String username;
+
+    private String password;
+
+    private String driverClassName;
+
+    private PoolConfig druid;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public PoolConfig getDruid() {
+        return druid;
+    }
+
+    public void setDruid(PoolConfig druid) {
+        this.druid = druid;
+    }
+}
+
+class PoolConfig {
+
+    private String initialSize;
+    private String minIdle;
+    private String maxActive;
+    private String maxWait;
+    private String timeBetweenEvictionRunsMillis;
+    private String minEvictableIdleTimeMillis;
+    private String validationQuery;
+    private String testWhileIdle;
+    private String testOnBorrow;
+    private String poolPreparedStatements;
+    private String filters;
+    private String connectionProperties;
+
+    public String getInitialSize() {
+        return initialSize;
+    }
+
+    public void setInitialSize(String initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public String getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(String minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public String getMaxActive() {
+        return maxActive;
+    }
+
+    public void setMaxActive(String maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public String getMaxWait() {
+        return maxWait;
+    }
+
+    public void setMaxWait(String maxWait) {
+        this.maxWait = maxWait;
+    }
+
+    public String getTimeBetweenEvictionRunsMillis() {
+        return timeBetweenEvictionRunsMillis;
+    }
+
+    public void setTimeBetweenEvictionRunsMillis(String timeBetweenEvictionRunsMillis) {
+        this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
+    }
+
+    public String getMinEvictableIdleTimeMillis() {
+        return minEvictableIdleTimeMillis;
+    }
+
+    public void setMinEvictableIdleTimeMillis(String minEvictableIdleTimeMillis) {
+        this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    public void setValidationQuery(String validationQuery) {
+        this.validationQuery = validationQuery;
+    }
+
+    public String getTestWhileIdle() {
+        return testWhileIdle;
+    }
+
+    public void setTestWhileIdle(String testWhileIdle) {
+        this.testWhileIdle = testWhileIdle;
+    }
+
+    public String getTestOnBorrow() {
+        return testOnBorrow;
+    }
+
+    public void setTestOnBorrow(String testOnBorrow) {
+        this.testOnBorrow = testOnBorrow;
+    }
+
+    public String getPoolPreparedStatements() {
+        return poolPreparedStatements;
+    }
+
+    public void setPoolPreparedStatements(String poolPreparedStatements) {
+        this.poolPreparedStatements = poolPreparedStatements;
+    }
+
+    public String getFilters() {
+        return filters;
+    }
+
+    public void setFilters(String filters) {
+        this.filters = filters;
+    }
+
+    public String getConnectionProperties() {
+        return connectionProperties;
+    }
+
+    public void setConnectionProperties(String connectionProperties) {
+        this.connectionProperties = connectionProperties;
+    }
+}
